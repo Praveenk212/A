3f491aa77f0732a8b0c5344bc90a5b67c493bfc9
Praveen Kumar <60648622+Praveenk212@users.noreply.github.com>
2020-03-06 10:24:25 +0530

    Add files via upload

---
+++ 12MVC_Jdbc/src/com/cg/web/dto/Emp.java
@@ -0,0 +1,41 @@
+package com.cg.web.dto;
+
+public class Emp {
+	
+	private int empId;
+	private String firstName;
+	private float salary;
+	
+	public Emp() {
+		super();
+	}
+	public Emp(int empId, String firstName, float salary) {
+		super();
+		this.empId = empId;
+		this.firstName = firstName;
+		this.salary = salary;
+	}
+	
+	
+	
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+}
